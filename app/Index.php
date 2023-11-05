e41711686430d5a28fa9537142557d22956a204d
sararun <sarosekvlad12@gmail.com>
2023-11-06 00:00:23 +0700

    init project

---
+++ app/Index.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App;
+
+class Index
+{
+    public $index ='index';
+}
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+namespace App;
+require_once __DIR__ . '/vendor/autoload.php';
+$a = new Index();
+
+echo $a->index;
