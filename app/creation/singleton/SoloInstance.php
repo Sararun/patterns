f28ab269f4f1a57d91262d17494d88045407c8a6
sararun <sarosekvlad12@gmail.com>
2023-11-07 00:11:24 +0700

    create singleton

---
+++ app/creation/singleton/SoloInstance.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\creation\singleton;
+
+class SoloInstance
+{
+    private array $props = [];
+
+    private static self $instance;
+
+    private function __construct()
+    {
+
+    }
+
+    public static function getInstance(): self
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+    public function setProperty(string $key, string $value): void
+    {
+        $this->props[$key] = $value;
+    }
+
+    public function getProperty(string $key): ?string
+    {
+        return $this->props[$key] ?? null;
+    }
+}
