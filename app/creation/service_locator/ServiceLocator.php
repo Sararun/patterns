f099848d23bbd49356fb3b4bffec5f2b6f36becd
sararun <sarosekvlad12@gmail.com>
2023-11-20 00:45:31 +0700

    create service locator

---
+++ app/creation/service_locator/ServiceLocator.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\creation\service_locator;
+
+use http\Exception\InvalidArgumentException;
+
+class ServiceLocator
+{
+    private array $services = [];
+
+    public function register(string $name, $service): void
+    {
+        $this->services[$name] = $service;
+    }
+
+    public function get(string $name)
+    {
+        return $this->services[$name] ?? throw new InvalidArgumentException('There is no such argument');
+    }
+}
